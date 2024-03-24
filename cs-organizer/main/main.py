80ecece93f0382b8134fd1631c084e6026d2fe5a
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-03-24 20:01:39 -0300

    fix(data): improve data processing (?)

--- cs-organizer/main/main.py
+++ cs-organizer/main/main.py
@@ -7,43 +7,59 @@ import logging
 import json
 import uuid
 
-SOURCE_PATH = pathlib.Path("source").absolute()
-DEST_PATH = pathlib.Path("dest").absolute()
+# Enviroments
+SOURCE_PATH = pathlib.Path(f"{os.path.dirname(__file__)}/source").absolute()
+DEST_PATH = pathlib.Path(f"{os.path.dirname(__file__)}/dest").absolute()
 CUSTOM_FORMATS_PATH = pathlib.Path(f"{SOURCE_PATH}/custom-formats").absolute()
 
+# Aplications
 applications = {
   str(uuid.uuid1()): {
-    "path": str(pathlib.Path(f"{CUSTOM_FORMATS_PATH}/sonarr").absolute()),
-    "files": {},
-    "database": {}
+    'path': str(pathlib.Path(f"{CUSTOM_FORMATS_PATH}/sonarr").absolute()),
+    'raw_data': {},
+    'cooked_data': {}
   },
   str(uuid.uuid1()): {
-    "path": str(pathlib.Path(f"{CUSTOM_FORMATS_PATH}/radarr").absolute()),
-    "files": {},
-    "database": {}
+    'path': str(pathlib.Path(f"{CUSTOM_FORMATS_PATH}/radarr").absolute()),
+    'raw_data': {},
+    'cooked_data': {}
   }
 }
 
+# Rawdata
 for _app_uuid in applications:
   for _app_files in os.listdir(applications[_app_uuid]['path']):
-    _files_uuid = str(uuid.uuid1())
-    _database_uuid = str(uuid.uuid1())
+    _file_uuid = str(uuid.uuid1())
+    applications[_app_uuid]['raw_data'][_file_uuid] = {}
 
-    applications[_app_uuid]['files'][_files_uuid] = {}
-    applications[_app_uuid]["database"][_database_uuid] = {}
+    applications[_app_uuid]['raw_data'][_file_uuid].update({'full_name': _app_files})
+    applications[_app_uuid]['raw_data'][_file_uuid].update({'full_path': str(pathlib.Path(f"{applications[_app_uuid]['path']}/{_app_files}"))})
+    applications[_app_uuid]['raw_data'][_file_uuid].update({'raw_data': json.load(open(pathlib.Path(f"{applications[_app_uuid]['path']}/{_app_files}"), 'r'))})
 
-    applications[_app_uuid]['files'][_files_uuid].update({"full_name": _app_files})
-    applications[_app_uuid]['files'][_files_uuid].update({"full_path": str(pathlib.Path(f"{applications[_app_uuid]['path']}/{_app_files}"))})
-    applications[_app_uuid]['files'][_files_uuid].update({"raw_data": json.load(open(pathlib.Path(f"{applications[_app_uuid]['path']}/{_app_files}"), 'r'))})
+# Processed data
+for _app_uuid in applications:
+  for _file_uuid in applications[_app_uuid]['raw_data']:
+    _item_uuid = str(uuid.uuid1())
+    applications[_app_uuid]['cooked_data'][_item_uuid] = {}
 
-    applications[_app_uuid]["database"][_database_uuid].update({"trash_id": applications[_app_uuid]['files'][_files_uuid]['raw_data']['trash_id']})
-    applications[_app_uuid]["database"][_database_uuid].update({"trash_colection": applications[_app_uuid]['files'][_files_uuid]['raw_data']['name'].split(': ')[-0]})
-    applications[_app_uuid]["database"][_database_uuid].update({"trash_name": applications[_app_uuid]['files'][_files_uuid]['raw_data']['name'].split(': ')[-1]})
-    applications[_app_uuid]["database"][_database_uuid].update({"file_collection": applications[_app_uuid]['files'][_files_uuid]['full_name'].split('-')[-0]})
-    applications[_app_uuid]["database"][_database_uuid].update({"file_name": applications[_app_uuid]['files'][_files_uuid]['full_name'].split('-')[-1].replace('.json','')})
+    applications[_app_uuid]['cooked_data'][_item_uuid].update({'trash_id': applications[_app_uuid]['raw_data'][_file_uuid]['raw_data']['trash_id']})
+    applications[_app_uuid]['cooked_data'][_item_uuid].update({'trash_colection': applications[_app_uuid]['raw_data'][_file_uuid]['raw_data']['name'].split(': ')[-0]})
+    applications[_app_uuid]['cooked_data'][_item_uuid].update({'trash_name': applications[_app_uuid]['raw_data'][_file_uuid]['raw_data']['name'].split(': ')[-1]})
+    applications[_app_uuid]['cooked_data'][_item_uuid].update({'file_collection': applications[_app_uuid]['raw_data'][_file_uuid]['full_name'].split('-')[-0]})
+    applications[_app_uuid]['cooked_data'][_item_uuid].update({'file_name': applications[_app_uuid]['raw_data'][_file_uuid]['full_name'].split('-')[-1].replace('.json','')})
     try:
-      applications[_app_uuid]["database"][_database_uuid].update({"format_score": applications[_app_uuid]['files'][_files_uuid]['raw_data']['trash_scores']['default']})
+      applications[_app_uuid]['cooked_data'][_item_uuid].update({'trash_scores': applications[_app_uuid]['raw_data'][_file_uuid]['raw_data']['trash_scores']['default']})
     except:
       pass
 
-print(json.dumps(applications,indent=2))
+json.dump(applications,open(f"{DEST_PATH}/dump.json", 'w'),indent=2)
+
+
+# On terminal run `main.py | sort -rn`
+for a in applications:
+  for b in applications[a]['cooked_data']:
+    # for c in applications[a]['cooked_data'][c]:
+    try:
+      print(f"{applications[a]['cooked_data'][b]['trash_scores']}.{applications[a]['cooked_data'][b]['file_collection']}.{applications[a]['cooked_data'][b]['file_name']}.json")
+    except:
+      pass
